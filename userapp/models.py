82f193379d28062f3c8617371c8ec85d0056db57
Artem P <pospeev.artem@icloud.com>
2023-01-21 18:05:44 +0400

    some changes

--- userapp/models.py
+++ userapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class CustomBaseModel(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created', editable=False)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated', editable=False)
+    deleted = models.BooleanField(default=False, verbose_name='Deleted')
+
+    class Meta:
+        abstract = True
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+
+class CustomUser(AbstractUser, CustomBaseModel):
+    email = models.EmailField(unique=True, verbose_name='Email')
+    birthday_date = models.DateTimeField(**NULLABLE, verbose_name='Birthday date')
+
+# TODO: описать сущность резюме после утверждения формы хранения и отображения
+# class Resume(CustomBaseModel):
+#     user = models.ManyToManyField(CustomUser, verbose_name='User')
