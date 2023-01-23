f92dc3fedfd8b47166d899153a850cf5ec9e0d16
Fortunella <sherbakovdaniil6@gmail.com>
2023-01-23 23:57:39 +0300

    1). Добавлены модели резюме и вакансии и связанные с ними модели. А именно: - ProfessionalRole - Employment - Schedule - Skill - LanguageLevel - Language - Resume - Experience - Сurrency - Vacancy
    2). Зарегистрированы в админке вышеперечисленные модели (пока минимально, чтобы просто их увидеть)

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from userapp.models import *
 
-# Register your models here.
+
+admin.site.register(Vacancy)
+admin.site.register(Resume)
+admin.site.register(Skill)
+admin.site.register(Employment)
+admin.site.register(Experience)
+admin.site.register(Language)
+admin.site.register(LanguageLevel)
+admin.site.register(ProfessionalRole)
+admin.site.register(Сurrency)
+admin.site.register(Schedule)
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'first_name', 'last_name', 'username', 'email', 'is_active', 'is_superuser', 'date_joined', 'last_login'
+    )
+    list_filter = ('is_active', 'is_superuser')
+    ordering = ('last_login', 'is_active',)
+    list_per_page = 20
+    search_fields = ('name_first', 'name_second', 'username', 'email')
+    actions = ('mark_as_delete', 'mark_as_active', 'mark_as_superuser', 'mark_as_user')
+
+    def mark_as_delete(self, request, queryset):
+        queryset.update(deleted=True)
+
+    mark_as_delete.short_description = 'Пометить удаленным'
+
+    def mark_as_active(self, request, queryset):
+        queryset.update(deleted=False)
+
+    mark_as_active.short_description = 'Снять пометку на удаление'
+
+    def mark_as_superuser(self, request, queryset):
+        queryset.update(is_superuser=True)
+
+    mark_as_superuser.short_description = 'Сделать суперпользователем'
+
+    def mark_as_user(self, request, queryset):
+        queryset.update(is_superuser=False)
+
+    mark_as_user.short_description = 'Сделать обычным пользователем'
--- userapp/models.py
+++ userapp/models.py
@@ -20,14 +20,87 @@ class CustomBaseModel(models.Model):
 
 
 class CustomUser(AbstractUser, CustomBaseModel):
-    email = models.EmailField(unique=True, verbose_name='Email')
+    email = models.EmailField(primary_key=True, unique=True, verbose_name='Email')
     birthday_date = models.DateField(**NULLABLE, verbose_name='Birthday date')
 
-# TODO: описать сущность резюме после утверждения формы хранения и отображения
-# class Resume(CustomBaseModel):
-#     user = models.ManyToManyField(CustomUser, verbose_name='User')
+
+class ProfessionalRole(CustomBaseModel):
+    name = models.CharField(primary_key=True, unique=True, verbose_name='Name', max_length=50)
 
 
-# TODO: описать сущность вакансий после получения и согласования данных по ним
-# class Vacancy(CustomBaseModel):
-#     pass
+class Employment(CustomBaseModel):
+    name = models.CharField(primary_key=True, unique=True, verbose_name='Name', max_length=50)
+
+
+class Schedule(CustomBaseModel):
+    name = models.CharField(primary_key=True, unique=True, verbose_name='Name', max_length=50)
+
+
+class Skill(CustomBaseModel):
+    name = models.CharField(primary_key=True, unique=True, verbose_name='Name', max_length=25)
+
+
+class LanguageLevel(CustomBaseModel):
+    name = models.CharField(primary_key=True, unique=True, verbose_name='Name', max_length=25)
+
+
+class Language(CustomBaseModel):
+    name = models.CharField(primary_key=True, unique=True, verbose_name='Name', max_length=25)
+    level = models.ForeignKey(LanguageLevel, verbose_name='Language level', on_delete=models.PROTECT)
+
+
+class Resume(CustomBaseModel):
+    user = models.ForeignKey(CustomUser, verbose_name='User', on_delete=models.CASCADE)
+    # Информация о желаемой позиции
+    professional_roles = models.ManyToManyField(ProfessionalRole, verbose_name='Position')
+    employment = models.ManyToManyField(Employment, verbose_name='Employment')
+    schedule = models.ManyToManyField(Schedule, verbose_name='Work schedule')
+    # Навыки
+    languages = models.ManyToManyField(Language, verbose_name='Language')
+    key_skills = models.ManyToManyField(Skill, verbose_name='Skills')
+    # Доп информация
+    about = models.TextField(verbose_name='About me')
+
+
+class Experience(CustomBaseModel):
+    resume = models.ForeignKey(Resume, verbose_name='Resume', related_name='experiences', on_delete=models.CASCADE)
+    name = models.CharField(primary_key=True, verbose_name='Name', max_length=50)
+    professional_roles = models.ForeignKey(ProfessionalRole, verbose_name='Position', on_delete=models.PROTECT)
+    description = models.TextField(verbose_name='Description', blank=True)
+    started_at = models.DateTimeField(auto_now_add=True, verbose_name='Started')
+    finished_at = models.DateTimeField(auto_now=True, verbose_name='Finished', blank=True)
+
+
+# Блок вакансий
+
+class Сurrency(CustomBaseModel):
+    name = models.CharField(primary_key=True, unique=True, verbose_name='Name', max_length=25)
+    short_name = models.CharField(unique=True, verbose_name='Short name', max_length=10)
+
+
+class Vacancy(CustomBaseModel):
+    # Информация о вакансии
+    vacancy_hh_id = models.PositiveIntegerField(primary_key=True, verbose_name='ID vacancy')
+    name = models.CharField(verbose_name='Name', max_length=50)
+    description = models.TextField(verbose_name='Description', blank=True)
+    area_name = models.CharField(verbose_name='Location', max_length=150)
+    # Деньги
+    salary_from = models.PositiveIntegerField(verbose_name='Salary from', default=0)
+    salary_to = models.PositiveIntegerField(verbose_name='Salary to', default=0)
+    currency = models.ForeignKey(Сurrency, verbose_name='Currency', related_name='vacancys', default=1, on_delete=models.SET_DEFAULT)
+    is_active = models.BooleanField(verbose_name='Active')
+    # Требования к кандидату
+    candidate_experience_months = models.PositiveIntegerField(verbose_name="Candidate's experience (months)", default=0)
+    schedule = models.ManyToManyField(Schedule, verbose_name='Work schedule')
+    employment = models.ManyToManyField(Employment, verbose_name='Employment')
+    branded_description = models.TextField(verbose_name='Description', blank=True)
+    key_skills = models.ManyToManyField(Skill, verbose_name='Skills')
+    professional_roles = models.CharField(verbose_name='Professional roles', max_length=50)
+    languages = models.ManyToManyField(Language, verbose_name='Language')
+    # Техническая информация
+    response_url = models.CharField(verbose_name='Response URL', max_length=250, blank=True)
+    published_hh_at = models.DateTimeField(verbose_name='Published on hh at')
+    created_hh_at = models.DateTimeField(verbose_name='Created on hh at')
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+
