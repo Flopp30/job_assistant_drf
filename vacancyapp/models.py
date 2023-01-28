4b923500a2f0b219ec22ec6dbff2fbe9c7817377
Fortunella <sherbakovdaniil6@gmail.com>
2023-01-28 16:52:17 +0300

    Создано приложение vacancyapp. В него перенесена модель Vacancy из приложения userapp
     Добавлены комментарии ко всем моделям

---
+++ vacancyapp/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from userapp.models import CustomBaseModel, Schedule, Employment, Skill, Language
+
+
+class Сurrency(CustomBaseModel):
+    """"Модель денежных валют (рубли, доллары и другие)."""
+
+    name = models.CharField(primary_key=True, unique=True, verbose_name='Name', max_length=25)  # Полное название валюты
+    short_name = models.CharField(unique=True, verbose_name='Short name', max_length=10)  # Короткое название валюты
+
+
+class Vacancy(CustomBaseModel):
+    """"Модель вакансии, созданной работодателем."""
+
+    # Информация о вакансии
+    vacancy_hh_id = models.PositiveIntegerField(primary_key=True, verbose_name='ID vacancy')  # ID вакансии
+    name = models.CharField(verbose_name='Name', max_length=50)  # Наименование вакансии
+    description = models.TextField(verbose_name='Description', blank=True)  # Описание вакансии
+    area_name = models.CharField(verbose_name='Location', max_length=150)  # Месторасположение работы из вакансии
+    # Деньги
+    salary_from = models.PositiveIntegerField(verbose_name='Salary from', default=0)  # Зарплата от
+    salary_to = models.PositiveIntegerField(verbose_name='Salary to', default=0)  # Зарплата до
+    currency = models.ForeignKey(Сurrency, verbose_name='Currency', related_name='vacancys', default=1, on_delete=models.SET_DEFAULT)  # Вид валюты
+    is_active = models.BooleanField(verbose_name='Active')  # Активна ли вакансия
+    # Требования к кандидату
+    candidate_experience_months = models.PositiveIntegerField(verbose_name="Candidate's experience (months)", default=0)  # Опыт работы кандидата (в месяцах)
+    schedule = models.ManyToManyField(Schedule, verbose_name='Work schedule')  # График работы (полный день, полставки)
+    employment = models.ManyToManyField(Employment, verbose_name='Employment')  # Вид работы (удаленка, стажировка, офис)
+    branded_description = models.TextField(verbose_name='Description', blank=True)
+    key_skills = models.ManyToManyField(Skill, verbose_name='Skills')  # Требуемые навыки от кандидата
+    professional_roles = models.CharField(verbose_name='Professional roles', max_length=50)  # Должность (начальник, повар)
+    languages = models.ManyToManyField(Language, verbose_name='Language')  # Языки
+    # Техническая информация
+    response_url = models.CharField(verbose_name='Response URL', max_length=250, blank=True)  # URL отклика
+    published_hh_at = models.DateTimeField(verbose_name='Published on hh at')  # Время публикации вакансии (на hh.ru)
+    created_hh_at = models.DateTimeField(verbose_name='Created on hh at')  # Время создания вакансии (на hh.ru)
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Created at')  # Время публикации вакансии (у нас)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')  # Время создания вакансии (у нас)
