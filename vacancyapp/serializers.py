4bca7df035cc2e1aded365629fa105479c5bf1f9
Artem <pospeev.artem@icloud.com>
2023-01-28 23:14:48 +0300

    Добавил сериализатор и вьюху к vacancyapp (#7)
    отрефакторил вьюху и сериализатор resumeapp
    
    Co-authored-by: Fortunella <sherbakovdaniil6@gmail.com>

---
+++ vacancyapp/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+from vacancyapp.models import Currency, Vacancy
+
+class CurrencyModelSerializer(ModelSerializer):
+    class Meta:
+        model = Currency
+        fields = '__all__'
+
+
+class VacancyModelSerializer(ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = '__all__'
--- vacancyapp/views.py
+++ vacancyapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin
+from vacancyapp.models import Currency, Vacancy
+from vacancyapp.serializers import CurrencyModelSerializer, VacancyModelSerializer
 
-# Create your views here.
+
+class CurrencyModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencyModelSerializer
+
+
+class VacancyModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancyModelSerializer
