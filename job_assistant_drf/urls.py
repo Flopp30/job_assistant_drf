3e098f7048290458447b5633ba8536a2321d8923
Fortunella <sherbakovdaniil6@gmail.com>
2023-01-28 22:35:14 +0300

    Добавил сериализатор и вьюху к resumeapp

--- job_assistant_drf/urls.py
+++ job_assistant_drf/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from userapp.views import CustomUserModelViewSet
+from resumeapp.views import ResumeModelViewSet
 
 router = DefaultRouter()
 router.register('users', viewset=CustomUserModelViewSet, basename='users')
+router.register('resume', viewset=ResumeModelViewSet, basename='professionalroles')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- resumeapp/views.py
+++ resumeapp/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin
+from resumeapp.models import ProfessionalRole, Employment, Schedule, Skill, LanguageLevel, Language, Resume, Experience
+from resumeapp.serializers import ProfessionalRoleModelSerializer, ScheduleModelSerializer, SkillModelSerializer, \
+    LanguageLevelModelSerializer, LanguageModelSerializer, ResumeModelSerializer, ExperienceModelSerializer
+
+
+class ProfessionalRoleModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin,
+                                   CreateModelMixin):
+    queryset = ProfessionalRole.objects.all()
+    serializer_class = ProfessionalRoleModelSerializer
+
+
+class EmploymentModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
+    queryset = Employment.objects.all()
+    serializer_class = ProfessionalRoleModelSerializer
+
+
+class ScheduleModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleModelSerializer
+
+
+class SkillModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
+    queryset = Skill.objects.all()
+    serializer_class = SkillModelSerializer
+
+
+class LanguageLevelModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
+    queryset = LanguageLevel.objects.all()
+    serializer_class = LanguageLevelModelSerializer
+
+
+class LanguageModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
+    queryset = Language.objects.all()
+    serializer_class = LanguageModelSerializer
+
+
+class ResumeModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
+    queryset = Resume.objects.all()
+    serializer_class = ResumeModelSerializer
+
+
+class ExperienceModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceModelSerializer
