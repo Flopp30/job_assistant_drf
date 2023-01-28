a2291df38e0d694112d1bf6e440d819dd2c7d2d9
Artem P <pospeev.artem@icloud.com>
2023-01-29 00:49:30 +0400

    add(fill testing data) added experience in testing data

--- userapp/management/commands/fill_testing_data.py
+++ userapp/management/commands/fill_testing_data.py
@@ -4,9 +4,10 @@ from random import choice
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from resumeapp.models import Employment, Schedule, Language, KeySkill, Resume
+from resumeapp.models import Employment, Schedule, Language, KeySkill, Resume, Experience
 from userapp.models import CustomUser
-from constant import ABOUT_ME, PROFESSIONAL_ROLES
+from constant import ABOUT_ME, PROFESSIONAL_ROLES, DESCRIPTIONS
+
 
 def load_from_json(file_name):
     with open(f'{settings.JSON_DIR}/{file_name}.json', 'r') as json_file:
@@ -42,9 +43,18 @@ def fill_resume():
         resume.key_skills.add(key_skill)
 
 
+def fill_experience():
+    resumes = Resume.objects.all()
+    previous_jobs_name = ['ЦИАН', 'EPAM', 'Google', 'Yandex', 'Mail', 'Rambler']
+    professional_role = PROFESSIONAL_ROLES
+    descriptions = DESCRIPTIONS
+    for resume, name, role, desc in zip(resumes, previous_jobs_name, professional_role, descriptions):
+        Experience.objects.create(resume=resume, name=name, professional_role=role, description=desc)
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         fill_users()
         fill_resume()
+        fill_experience()
