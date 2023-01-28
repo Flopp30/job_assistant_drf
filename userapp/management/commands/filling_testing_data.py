7c88576187e73797ed85828a049a0138e5c9ccdf
Artem P <pospeev.artem@icloud.com>
2023-01-29 00:13:45 +0400

    feat(filling testing data)

--- userapp/management/commands/filling_testing_data.py
+++ userapp/management/commands/filling_testing_data.py
@@ -1,10 +1,12 @@
 import json
+from random import choice
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
+from resumeapp.models import Employment, Schedule, Language, KeySkill, Resume
 from userapp.models import CustomUser
-
+from constant import ABOUT_ME, PROFESSIONAL_ROLES
 
 def load_from_json(file_name):
     with open(f'{settings.JSON_DIR}/{file_name}.json', 'r') as json_file:
@@ -19,21 +21,30 @@ def fill_users():
         CustomUser.objects.create_user(**user)
 
 
-PROFESSIONAL_ROLES = [
-    'Python backend developer',
-    'JS developer',
-    'C# разработчик',
-    'Аналитик данных',
-    'Java разработчик',
-    'Fullstack python dev',
-]
-
-
 def fill_resume():
     users = CustomUser.objects.all()
+    employments = Employment.objects.all()
+    schedules = Schedule.objects.all()
+    languages = Language.objects.all()
+    key_skills = KeySkill.objects.all()
+
+    Resume.objects.all().delete()
+    for about_me, prof_role in zip(ABOUT_ME, PROFESSIONAL_ROLES):
+        user = choice(users)
+        employment = choice(employments)
+        schedule = choice(schedules)
+        language = choice(languages)
+        key_skill = choice(key_skills)
+        resume = Resume.objects.create(user=user, professional_role=prof_role, about=about_me)
+        resume.employment.add(employment)
+        resume.schedule.add(schedule)
+        resume.languages.add(language)
+        resume.key_skills.add(key_skill)
+
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         fill_users()
+        fill_resume()
