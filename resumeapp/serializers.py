01df688e3790d31386b2de2c37c36cf39e1afac7
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-29 18:51:18 +0300

    Class docstring? line 9, 11, 17, 19, 25, 27, 33, 35, 41, 43, 49, 54, 60, 62, 68, 78 serializers.py

--- resumeapp/serializers.py
+++ resumeapp/serializers.py
@@ -6,51 +6,66 @@ from userapp.serializers import CustomUserModelSerializer
 
 
 class EmploymentModelSerializer(ModelSerializer):
+    """Class docstring?"""
     class Meta:
+        """Class docstring?"""
         model = Employment
         fields = '__all__'
 
 
 class ScheduleModelSerializer(ModelSerializer):
+    """Class docstring?"""
     class Meta:
+        """Class docstring?"""
         model = Schedule
         fields = '__all__'
 
 
 class KeySkillModelSerializer(ModelSerializer):
+    """Class docstring?"""
     class Meta:
+        """Class docstring?"""
         model = KeySkill
         fields = '__all__'
 
 
 class LanguageLevelModelSerializer(ModelSerializer):
+    """Class docstring?"""
     class Meta:
+        """Class docstring?"""
         model = LanguageLevel
         fields = '__all__'
 
 
 class LanguageModelSerializer(ModelSerializer):
+    """Class docstring?"""
     class Meta:
+        """Class docstring?"""
         model = Language
         fields = '__all__'
 
 
 class ResumeLanguageLevelModelSerializer(ModelSerializer):
+    """Class docstring?"""
     language = LanguageModelSerializer()
     level = LanguageLevelModelSerializer()
 
     class Meta:
+        """Class docstring?"""
         model = ResumeLanguageLevel
         fields = '__all__'
 
 
 class ExperienceModelSerializer(ModelSerializer):
+    """Class docstring?"""
     class Meta:
+        """Class docstring?"""
         model = Experience
         fields = '__all__'
 
 
 class ResumeModelSerializer(ModelSerializer):
+    """Class docstring?"""
     schedule = ScheduleModelSerializer(many=True)
     employment = EmploymentModelSerializer(many=True)
     user = CustomUserModelSerializer()
@@ -60,5 +75,6 @@ class ResumeModelSerializer(ModelSerializer):
 
 
     class Meta:
+        """Class docstring?"""
         model = Resume
         fields = '__all__'
