d6b6bb83b9f365df522c09753007348570424819
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-29 18:34:33 +0300

    Class docstrings? line 6 apps.py

--- resumeapp/apps.py
+++ resumeapp/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 
 class ResumeappConfig(AppConfig):
+    """Class docstrings?"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'resumeapp'
     verbose_name = 'Резюме'
