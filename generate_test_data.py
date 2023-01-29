50db15c5abcd0307c3ca96c1e0a17194f8b39eb5
Андрей Сапунов <jokerjam@yandex.ru>
2023-01-29 14:40:19 +0300

    Generation ready

--- generate_test_data.py
+++ generate_test_data.py
@@ -1,5 +1,10 @@
 # TODO здесь Андрей пишет заполнения тестовыми данными JSON'ов. Путь /json/*
+import string
+
 import json
+import random as rnd
+from datetime import timedelta, datetime
+
 
 # Класс тестовых данных, сериализуется в json
 class TestData:
@@ -10,9 +15,8 @@ class TestData:
     email = ""
     birthday_date = ""
 
+
 # класс генерации файла с тестовыми данными
-# len_passw         - длинна генерируемого пароля
-# email_template    - основа для генерации email адреса
 class GenTestData:
     # шаблон для формирования username
     user_names = (
@@ -27,7 +31,8 @@ class GenTestData:
     first_names_m = (
         "Artem", "Aleksey", "Andrey", "Anton", "Afanasiy", "Alexandr", "Anatoliy",
         "Boris", "Boleslav", "Bogdan", "Vasiliy", "Vaicheslav", "Vsevolod", "Vladimir", "Viktor",
-        "Georgiy", "Gennadiy", "Gleb", "German", "Dmitriy", "Denis", "Daniil", "Evgeniy", "Egor", "Jora", "Zenon", "Zakhar",
+        "Georgiy", "Gennadiy", "Gleb", "German", "Dmitriy", "Denis", "Daniil", "Evgeniy", "Egor", "Jora", "Zenon",
+        "Zakhar",
         "Igor", "Ibragim", "Ivan", "Ignat", "Konstantin", "Kirill", "Kondrat", "Lev", "Leonid", "Lavrentiy", "Makar",
         "Maksim", "Matvey", "Marat", "Miron", "Mikhail", "Murat", "Nikolay", "Nikodim", "Natan", "Nazar",
         "Oleg", "Osip", "Pavel", "Petr", "Potap", "Ruslan", "Robert", "Rail", "Rustam", "Rinat", "Sergey", "Stanislav",
@@ -37,8 +42,10 @@ class GenTestData:
 
     # шаблон для формирования женских фио
     first_names_f = (
-        "Alla", "Alina", "Alevtina", "Anna", "Anastasya", "Agata", "Alena", "Broneslava", "Barbara", "Viktorya", "Valentina",
-        "Vasilina", "Vasilisa", "Vera", "Galina", "Gabriella", "Gera", "Gertruda", "Diana", "Darya", "Elena", "Elizaveta",
+        "Alla", "Alina", "Alevtina", "Anna", "Anastasya", "Agata", "Alena", "Broneslava", "Barbara", "Viktorya",
+        "Valentina",
+        "Vasilina", "Vasilisa", "Vera", "Galina", "Gabriella", "Gera", "Gertruda", "Diana", "Darya", "Elena",
+        "Elizaveta",
         "Ekaterina", "Evgenya", "Jozefina", "Janna", "Zoya", "Zara", "Zarina", "Irina", "Klavdya", "Klementina",
         "Karina", "Lera", "Laura", "Lana", "Lusia", "Ludmila", "Lara", "Larisa", "Marya", "Marta", "Natalya", "Olga",
         "Oksana", "Polina", "Paranya", "Pelageya", "Rita", "Raisa", "Ruslana", "Roza", "Rimma", "Svetlana", "Stefanya",
@@ -63,41 +70,74 @@ class GenTestData:
     )
 
     # формирование тестового файла
-    def __init__(self, len_passw=5, email_template='test'):
-        self.len_passw = len_passw
-        self.email_template = email_template
+    def __init__(self, len_passw=5):
+        self.len_passw = len_passw  # длина пароля
+        self.dict_names = {}
+
+    # генерация словаря использования user_name
+    def _dict_gen(self):
+        self.dict_names = {x: 0 for x in self.user_names}
 
     # Формирование файла с тестовыми данными
-    # file_name - имя файла с генерируемыми данными, по умолчанию test.json в каталоге
-    def generate_json(self, file_name='/json/test.json'):
-        pass
+    # file_name     - имя файла с генерируемыми данными
+    # count_person  - кол-во генерируемых записей (по умолчанию 500)
+    # restart       - сбросить счетчик аккаунтов для предыдущей генерации
+    def generate_json(self, file_name, count_person, restart):
+        data_list = []
+
+        # сформировать словарь генерации, если указано или словарь пуст
+        if restart or len(self.dict_names) == 0:
+            self._dict_gen()
+
+        for i in range(0, count_person):
+            data = TestData()
+            user_name = self._get_username()
+            data.first_name, data.last_name = self._get_name()
+            data.username = user_name
+            data.email = user_name + "@mail.ru"
+            data.password = self._get_password()
+            data.birthday_date = self._get_birthday_date()
+
+            data_list.append(data)
+
+        try:
+            with open(file_name, "w") as json_file:
+                json.dump(data_list, json_file, default=lambda x: x.__dict__, sort_keys=True, indent=2)
+        except:
+            print("Write json-file error!")
 
     # генерация username
     def _get_username(self):
-        pass
+        user_name = rnd.choice(self.user_names)
+        count = self.dict_names[user_name]
+        count += 1
+        self.dict_names[user_name] = count
+        return f'{user_name}{count}'
 
     # генерация пароля
     def _get_password(self):
-        pass
+        return ''.join(rnd.choice(string.ascii_letters) for i in range(self.len_passw))
 
-    # генерация first_name
-    def _get_first_name(self):
-        pass
 
-    # генерация last_name
-    def _get_last_name(self):
-        pass
+    # генерация имени, возвращает first_name, last_name
+    def _get_name(self):
+        # определим пол
+        gender = rnd.choice(('m', 'f'))
+        if gender == 'm':
+            return rnd.choice(self.first_names_m), rnd.choice(self.last_names)
 
-    # генерация
-    def _get_email(self):
-        pass
+        return rnd.choice(self.first_names_f), rnd.choice(self.last_names) + "a"
 
     # генерация
     def _get_birthday_date(self):
-        pass
+        days_diff = timedelta(rnd.choice(range(20, 60)) * 365 + rnd.randint(0, 365))
+        date_new = datetime.now() - days_diff
+        return date_new.strftime("%Y-%m-%d")
+
 
 def main():
-    pass
+    generator = GenTestData()
+    generator.generate_json("json/testdata.json", 100, True)
 
 
 if __name__ == '__main__':
