d927a8a82339a7da279a953838d562636dd4981d
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-29 19:20:19 +0300

    Class docstring? line 6 apps.py

--- userapp/apps.py
+++ userapp/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 
 class UserappConfig(AppConfig):
+    """Class docstring?"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userapp'
     verbose_name = 'Пользователи'
