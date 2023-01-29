80f21ec9e726d1cfd5859ebb7180e7373e90c58c
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-29 19:34:26 +0300

    Line too long. line 3, 11, 17, 23 views.py

--- userapp/views.py
+++ userapp/views.py
@@ -1,5 +1,6 @@
 """Module docstring?"""
 from rest_framework.viewsets import GenericViewSet
+"""Line 3 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin
 
 from userapp.serializers import LanguageLevelModelSerializer, LanguageModelSerializer
@@ -7,19 +8,19 @@ from userapp.models import CustomUser
 from userapp.serializers import CustomUserModelSerializer
 from resumeapp.models import LanguageLevel, Language
 
-
+"""Line 11 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 class CustomUserModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
     """Class docstring?"""
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserModelSerializer
 
-
+"""Line 17 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 class LanguageLevelModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
     """Class docstring?"""
     serializer_class = LanguageLevelModelSerializer
     queryset = LanguageLevel.objects.all()
 
-
+"""Line 23 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 class LanguageModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
     """Class docstring?"""
     queryset = Language.objects.all()
