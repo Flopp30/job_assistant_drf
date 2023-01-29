a62d11c7b35796955820a1b20a6bb25c168f5fdf
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-29 19:04:42 +0300

    Line 14 too long. line 14 create_superuser.py

--- userapp/management/commands/create_superuser.py
+++ userapp/management/commands/create_superuser.py
@@ -10,4 +10,5 @@ class Command(BaseCommand):
     ''' Создает суперпользователя admin/admin'''
     def handle(self, *args, **options):
         if os.getenv('ENV_TYPE') == 'local':
+            """Line 14 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
             CustomUser.objects.create_superuser('admin', password='admin', email='admin@django.local')
