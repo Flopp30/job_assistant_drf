ede2fb63bc7bc0a5d6894e0b7f445aa4323211fa
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-29 19:01:07 +0300

    Line too long. line 3, 12, 24, 30, 36 views.py

--- resumeapp/views.py
+++ resumeapp/views.py
@@ -1,5 +1,6 @@
 """Module docstrings?"""
 from rest_framework.viewsets import GenericViewSet
+"""Line 4 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin
 
 from resumeapp.models import Employment, Schedule, KeySkill, Resume, Experience
@@ -8,31 +9,31 @@ from resumeapp.serializers import (
     ExperienceModelSerializer, EmploymentModelSerializer
 )
 
-
+"""Line 12 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 class EmploymentModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
     """Class docstring?"""
     queryset = Employment.objects.all()
     serializer_class = EmploymentModelSerializer
 
-
+"""Line 17 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 class ScheduleModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
     """Class docstring?"""
     queryset = Schedule.objects.all()
     serializer_class = ScheduleModelSerializer
 
-
+"""Line 24 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 class SkillModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
     """Class docstring?"""
     queryset = KeySkill.objects.all()
     serializer_class = KeySkillModelSerializer
 
-
+"""Line 30 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 class ResumeModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
     """Class docstring?"""
     queryset = Resume.objects.all()
     serializer_class = ResumeModelSerializer
 
-
+"""Line 36 too long. https://peps.python.org/pep-0008/#maximum-line-length"""
 class ExperienceModelViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin):
     """Class docstring?"""
     queryset = Experience.objects.all()
