d322ce610a846e0298394db334b5c694506c4c93
Artem <pospeev.artem@icloud.com>
2023-02-08 23:13:35 +0400

    add(auth using tokens

--- job_assistant_drf/urls.py
+++ job_assistant_drf/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 from userapp.views import CustomUserModelViewSet
 from resumeapp.views import ResumeModelViewSet
@@ -9,10 +10,13 @@ from vacancyapp.views import VacancyModelViewSet
 router = DefaultRouter()
 router.register('users', viewset=CustomUserModelViewSet, basename='users')
 router.register('resume', viewset=ResumeModelViewSet, basename='resume')
-router.register('vacancys', viewset=VacancyModelViewSet, basename='vacancys')
+router.register('vacancies', viewset=VacancyModelViewSet, basename='vacancies')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # auth
+    path('api-auth-token/', views.obtain_auth_token),
 ]
